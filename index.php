451797c4081119af6d658267e378e07a78062402
bobby-petrov <bobby.petrov@primenetul.com>
2015-08-28 13:59:39 +0100

    meta tag added

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/styles.css">
 	<title>GitHub Test Page</title>
 </head>
 <body>
-	
+	<div class="container">
+		
+	</div>
 </body>
 </html>
